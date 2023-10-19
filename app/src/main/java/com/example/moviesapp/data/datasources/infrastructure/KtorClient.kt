3f71ab5dbe9841eac40c373619af23952e590622
Alvaro Prado <alvaroprado@tokenlab.com.br>
2023-10-19 15:08:36 -0400

    refact: update KtorClient.kt

--- app/src/main/java/com/example/moviesapp/data/datasources/infrastructure/KtorClient.kt
+++ app/src/main/java/com/example/moviesapp/data/datasources/infrastructure/KtorClient.kt
@@ -1,9 +1,17 @@
 package com.example.moviesapp.data.datasources.infrastructure
 
+import android.util.Log
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -11,12 +19,28 @@ import kotlinx.serialization.json.Json
 private const val TIME_OUT = 60_000
 
 val ktorHttpClient = HttpClient(Android) {
-    install(Logging)
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+                Log.v("KtorHttpClient", message)
+            }
+        }
+        level = LogLevel.ALL
+    }
     install(ContentNegotiation) {
         json(Json {
             prettyPrint = true
             isLenient = true
+            ignoreUnknownKeys = true
         })
+    }
+    install(ResponseObserver) {
+        onResponse {
+            Log.d("HTTP status:", "${it.status.value}")
+        }
+    }
+    install(DefaultRequest) {
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
     }
 
 }
